a589ea998cabc7ca7f86b9c23b40677526d6022f
Luis Maldonado <maldonadovla@gmail.com>
2019-07-05 12:01:07 -0600

    Added env file and settings

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
-const labelPrint =  require('./labels/eplLabelPrinter');
-const bodyParser = require('body-parser')
-const port = 25639
+const cors = require('cors');
+const labelPrint = require('./labels/eplLabelPrinter');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const port = process.env.SERVER_PORT;
+
+const whitelist = [process.env.DEV_SITE, process.env.PRODUCTION_SITE, process.env.LOCAL_SITE];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
 
 app.use(bodyParser.json()) // for parsing application/json body rquests
 
-app.post('/printLabel', function (req, res) {
+app.post('/printLabel', cors(corsOptions), function (req, res) {
   try {
     labelPrint(req.body);
-    res.send('Label print dispatch!')  
+    res.send('Label print dispatch!');
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
   }
 })
 
-app.listen(port, () => console.log(`Print label app listening on port ${port}!`))
+app.listen(port, () => console.log(`Print label app listening on port ${port}!`));
